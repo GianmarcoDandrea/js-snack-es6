2fe83fa468d165c2250218a47ad870464a7c2b5e
GianmarcoDandrea <gianm96@hotmail.it>
2023-11-09 10:39:39 +0100

    add: snack bonus corrected in class

--- SNACK BONUS/js/script.js
+++ SNACK BONUS/js/script.js
@@ -1,13 +1,26 @@
 // SNACK BONUS
-const numbersArray = [23, 14, 6, 8, 10, 25, 85, 58, 74, 9, 5, 7, 12];
-console.log(numbersArray);
+const myArray = [23, 14, 6, 8, 10, 25, 85, 58, 74, 9, 5, 7, 12];
+const min = 1;
+const max = 4;
 
-let newArray = getSubArray(numbersArray, 5, 9)
+let newArray = getSubArray(myArray, min, max)
 console.log(newArray);
 
-function getSubArray(array, a, b) {
 
+//soluzione con foreach
+function getSubArray(array, min, max) {
+    const result=[]
+    array.forEach((elem, index) => {
+        if( index >= min && index <= max){
+            result.push(elem)
+        }
+    });
 
     
-    return newArray
+    return result
 }
+
+// //soluzione con filter
+// function getSubArray(array, min, max) {
+//     return newArray.filter((elem, index) => index >= min && index <= max)
+// }
